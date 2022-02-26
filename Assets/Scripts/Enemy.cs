df2a7624eaf23a4cba4c43daed17afbe3849afd7
ZEYNEP <zeynepclktn@gmail.com>
2022-02-26 23:38:26 +0300

    attack and minion enemy type finished

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Animations
+    private Animator animator;
+    private string currentState;
+    const string idle = "EnemyIdle";
+    const string hit = "EnemyHit";
+    const string attack = "EnemyAttack";
+    const string death = "EnemyDeath";
+    
+    
+    //Move
+    private Rigidbody2D rb;
+    Vector3 movement;
+    public float speed = 5;
+    bool Moveright = false;
+
+    //Hit
+    public int maxHealth = 100;
+    public int currentHealth;
+
+
+    //Attack
+    public float CalculatedTime;
+    public float TimeBtwEachShot;
+    public GameObject Fireball;
+    bool playerOnline = false;
+    public Transform PlayerPosition;
+    public float minimumFiringDistance;
+    public float maxFiringDistance;
+    public float damage = 12.5f;
+    float totalDamage;
+    bool playerAlive = true;
+    float cr;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+        currentHealth = maxHealth;
+        CalculatedTime = TimeBtwEachShot; 
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //ChangeAnimationState(idle);
+    }
+
+    void Update()
+    {
+        if(playerAlive)
+            CheckAttack();
+        AutoMove();
+        CheckPlayerDead();
+        //ChangeAnimations();
+    }
+    void CheckPlayerDead()
+    {
+        if (totalDamage == 100) //player health
+        {
+            playerOnline = false;
+            playerAlive = false;
+        }
+    }
+   void CheckAttack()
+    {
+        if (Vector2.Distance(transform.position, PlayerPosition.position) <= minimumFiringDistance)
+        {
+            if(!playerOnline) cr = transform.position.x;
+            playerOnline = true;          
+            if (Moveright) { transform.Rotate(0f, 180f, 0f); Moveright = false; }
+            AttackMexhanism();
+        }
+        else
+        {
+            playerOnline = false;
+        }
+    }
+    void AutoMove()
+    {
+        if (!playerOnline)
+        {
+            if (Moveright)
+            {
+                movement = new Vector3(2, 0f, 0f);
+                transform.position = transform.position + movement * Time.deltaTime;
+            }
+            else
+            {
+                movement = new Vector3(-2, 0f, 0f);
+                transform.position = transform.position + movement * Time.deltaTime;
+            }
+        }
+    }
+   
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.CompareTag("turn")&& !playerOnline)
+        {
+            if (Moveright) Moveright = false;
+            else Moveright = true;
+            transform.Rotate(0f, 180f, 0f);
+        }
+        if (trig.CompareTag("Player"))
+        {       
+            trig.transform.SendMessage("DamagePlayer", damage);
+            totalDamage += damage;
+        }
+
+    }
+    void AttackMexhanism()
+    {
+            if (CalculatedTime <= 0)
+            {               
+                movement = new Vector3((PlayerPosition.position.x+0.5f), -2.5f, 0f);
+                transform.position = movement;
+                animator.SetBool("Attack", true);
+                CalculatedTime = TimeBtwEachShot;
+            }
+            else
+            {
+                movement = new Vector3(cr,-2.5f, 0f);
+                transform.position = movement;
+                CalculatedTime -= Time.deltaTime;
+                animator.SetBool("Attack", false);
+            }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+    void ChangeAnimations()
+    {
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("Hurt");
+        if (currentHealth <= 0)
+        {
+            animator.SetBool("Alive", false);
+            Die();           
+        }
+    }
+    void Die()
+    {
+        ChangeAnimationState(death);
+        movement = new Vector3(transform.position.x, -3.87f, transform.position.z);
+        transform.position = movement;
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -28,7 +29,27 @@ public class PlayerController : MonoBehaviour
     const string run = "PlayerRun";
     const string jump = "PlayerJump";
     const string fall = "PlayerFall";
-    
+    const string attack1 = "PlayerAttack1";
+    const string attack2 = "PlayerAttack2";
+    const string attack3 = "PlayerAttack3";
+    const string hit = "PlayerHit";
+    const string death = "PlayerDeath";
+    public float exitTime;
+
+    //Attack
+    float attack_time = 0.0f;
+    int attack_count = 0;
+    public float CurrentHealth = 100f;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public int attackDamage = 10;
+    public LayerMask enemyLayers;
+    bool attackPressed = false;
+
+    //Die
+    Vector3 movement;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -40,7 +61,9 @@ public class PlayerController : MonoBehaviour
         CheckState();        
         CheckInputs();
         ChangeAnimations();    
-        FlipPlayer();  
+        FlipPlayer();
+        CheckIfAttack();
+        attack_time += Time.deltaTime;
     }
 
     void FixedUpdate() 
@@ -107,8 +130,9 @@ public class PlayerController : MonoBehaviour
                 ChangeAnimationState(jump);
             if(rb.velocity.y < 0)
                 ChangeAnimationState(fall);    
-        } 
-    }
+        }
+   
+     }
     void ChangeAnimationState(string newState)
     {
         if(currentState == newState) return;
@@ -118,5 +142,68 @@ public class PlayerController : MonoBehaviour
     void OnDrawGizmos() 
     {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+    void CheckIfAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            attackPressed = true;
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        if (attackPressed)
+        {
+            attackDamage += 2;
+            attack_count++;
+            if (attack_count > 3)
+            {
+                attack_count = 1;
+                attackDamage = 10;
+            }
+
+            if (attack_time > 1.0f)
+            {
+                attack_count = 1;
+                attackDamage = 10;
+            }
+
+            animator.SetTrigger("Attack" + attack_count);
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+
+            attack_time = 0f;
+           
+        }
+
+    }
+    public virtual void DamagePlayer(float amount)
+    {
+        CurrentHealth -= amount;
+        animator.SetTrigger("Hit");
+        if (CurrentHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        ChangeAnimationState(death);
+        movement = new Vector3(transform.position.x, -3.83f, transform.position.z);
+        transform.position = movement;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+
+    private void OnDrawGizmosSelected() 
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
