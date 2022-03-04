a9accd78afb3155f5c3b5f993cd8618719c61e42
Nakataha <ambnakata@gmail.com>
2022-03-04 14:16:42 +0300

    no message

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -24,6 +24,7 @@ public class LevelManager : MonoBehaviour
     public void DeathDefiance()
     {
         player.transform.position = player.transform.position;
+
     }
 
     public void RespawnPlayer()
