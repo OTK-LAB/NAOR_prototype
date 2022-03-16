15e29367cf6139d5b75c6da2a59fec9e70934ccf
IremK <ayseirem.kayacioglu@gmail.com>
2022-03-16 22:42:18 +0300

    NOT FINISHED Added random quotes at the corner. Added Start,Continue and Exit Screens.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject IntroductionMenu;
     public GameObject MainMenu;
-
+    public GameObject Menu;
+    public GameObject StartMenu;
+    public GameObject ContinueMenu;
+    public GameObject SettingsMenu;
+    public GameObject ExitMenu;
+    public GameObject QuoteText;
+    int RandNum;
     public void Start()
     {
-        
+        GenerateQuote();
     }
 
 
@@ -22,14 +29,41 @@ public class MainMenuManager : MonoBehaviour
         }
     }
 
-    public void StartGame()
+    public void StartScreen()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Loads the next scene in line
+        Menu.SetActive(false);
+        StartMenu.SetActive(true);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Loads the next scene in line
     }
 
-    public void ExitGame()
+    public void Continue()
+    {
+        ContinueMenu.SetActive(true);
+        Menu.SetActive(false);
+    }
+    public void Settings()
+    {
+        SettingsMenu.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void ExitScreen()
+    {
+        ExitMenu.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void QuitGame()
     {
         Application.Quit();
     }
 
+    public void GenerateQuote()
+    {
+        RandNum = Random.Range(0, 3);
+        string[] Quotes = { "Face the bars.Solve the lock. Defeat the cage.\n\tRun to hands of freedom.(laughs)\nKahve",
+                            "But we are different kid. We are nnot fighting with cages.\n\tOur revenge oath is to destroy the cagemaker.\nKahve",
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n\tsed do eiusmod tempor incididunt ut labore et Kahve" };
+        QuoteText.GetComponent<TMPro.TextMeshProUGUI>().text = Quotes[RandNum];
+    }
 }
