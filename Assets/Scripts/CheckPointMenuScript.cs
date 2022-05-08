a01f7ef404e1ced6d4e0b5187015ed4133420758
IremK <ayseirem.kayacioglu@gmail.com>
2022-05-09 00:33:08 +0300

    Checkpoint menu opens when "U" is pressed next to a checkpoint

---
+++ Assets/Scripts/CheckPointMenuScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointMenuScript : MonoBehaviour
+{
+    public GameObject CheckPointMenu;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            OpenCheckPointMenu();
+        }
+    }
+    void OpenCheckPointMenu()
+    {
+        CheckPointMenu.SetActive(true);
+        TogglePause();
+    }
+
+    public void CloseCheckPointMenu()
+    {
+        TogglePause();
+        CheckPointMenu.SetActive(false);  
+    }
+    void TogglePause()
+    {
+        if(Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+        }
+
+        else if(Time.timeScale == 1f)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
