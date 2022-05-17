c9fa59911cef98560d45a368d92cd1e7a95662f4
ahmetefeserefli <ahmetefe.serefli@ogr.iuc.edu.tr>
2022-05-17 12:13:05 +0300

    no message

--- Assets/Scripts/Enemy Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealthManager.cs
@@ -41,64 +41,48 @@ public class EnemyHealthManager : MonoBehaviour
         }
 
 
-        public Enemy (float GivenMaxHealth , Rigidbody Givenrgb )
+        /*public Enemy (float GivenMaxHealth , Rigidbody Givenrgb )
         {
             MaxHealth = GivenMaxHealth;
             rgb = Givenrgb;
         }
 
+        public Enemy(string x)
+        {
+            
+            switch (x)
+            {
+                case minion:
+                    MaxHealth = 1;
+                    
+
+                case minion2:
+                    MaxHealth = 1;
+
+                case preacher:
+                    MaxHealth = 1;
+
+                case legolas:
+                    MaxHealth = 1;
+
+                case þovalye:
+                    MaxHealth = 1;
+
+                case ultimateþovalye:
+                    MaxHealth = 1;
+
+                case villager:
+                    MaxHealth = 1;
+
+                case mercenarie:
+                    MaxHealth = 1;
+            }
+            
+
+        }*/
 
 
-        
     }
 
-   /* public class Minion : Enemy
-    {       
-        MaxHealth = 40;
-        Regen = 1;
-    }
-
-    public class Minion2 : Enemy
-    {
-        MaxHealth = 20;
-        Regen = 1;
-    }
-
-    public class preacher : Enemy
-    {
-        MaxHealth = 60;
-        Regen = 1;
-    }
-
-    public class Legolas : Enemy
-    {
-        MaxHealth = 60;
-        Regen = 1;
-    }
-
-    public class Sovalye : Enemy
-    {
-        MaxHealth = 110;
-        Regen = 1;
-    }
-
-    public class UltimateSovalye : Enemy
-    {
-        MaxHealth = 350;
-        Regen = 1;
-    }
-
-    public class Villager : Enemy
-    {
-        MaxHealth = 10;
-        Regen = 1;
-    }
-
-    public class Mercenarie : Enemy
-    {
-        MaxHealth = 110;
-        Regen = 1;
-    }
-*/
 
 }
