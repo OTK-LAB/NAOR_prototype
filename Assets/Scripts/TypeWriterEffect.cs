98f1c5b0a9e5623259311af4d4097f73966415ac
IremK <ayseirem.kayacioglu@gmail.com>
2022-05-25 12:55:54 +0300

    NAOR logosu sola kaydırıldı

--- Assets/Scripts/TypeWriterEffect.cs
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -7,7 +7,7 @@ public class TypeWriterEffect : MonoBehaviour {
 	//public float delay = 0.1f;
 	//public string fullText;
 	//private string currentText = "";
-	public float time = 0.1f;
+	public float time=0.5f;
 	// Use this for initialization
 	void OnEnable() {
 		
@@ -17,6 +17,7 @@ public class TypeWriterEffect : MonoBehaviour {
 	
 	IEnumerator Fade()
     {
+		
         for(float f = 0; f <= 2; f += Time.deltaTime)
         {
             this.GetComponent<TextMeshProUGUI>().alpha = Mathf.Lerp(0f, 1f, f / 2);
@@ -24,12 +25,12 @@ public class TypeWriterEffect : MonoBehaviour {
         }
         this.GetComponent<TextMeshProUGUI>().alpha = 1;
 		yield return new WaitForSecondsRealtime(time);
-		for (float f = 0; f <= 2; f += Time.deltaTime)
-		{
-			this.GetComponent<TextMeshProUGUI>().alpha = Mathf.Lerp(1f, 0f, f / 2);
-			yield return null;
-		}
-		this.GetComponent<TextMeshProUGUI>().alpha = 0;
+			for (float f = 0; f <= 2; f += Time.deltaTime)
+			{
+				this.GetComponent<TextMeshProUGUI>().alpha = Mathf.Lerp(1f, 0f, f / 2);
+				yield return null;
+			}
+			this.GetComponent<TextMeshProUGUI>().alpha = 0;
 	}
 	
 }
