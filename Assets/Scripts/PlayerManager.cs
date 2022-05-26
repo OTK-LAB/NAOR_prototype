2304459a048c6d5830d93ad5bfde384c8b21f3b2
sinanscebeci <sinanselcukcebeci@ogr.iu.edu.tr>
2022-05-26 15:48:56 +0300

    Death animation sprites changed.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -15,7 +15,7 @@ public class PlayerManager : MonoBehaviour
 
     public static PlayerManager instance;
 
-    private int lives = 2;
+    private int lives = 4;
     public float MaxHealth = 100;
     public float CurrentHealth = 100f;
     public bool isHealing;
@@ -31,6 +31,7 @@ public class PlayerManager : MonoBehaviour
     //Animations
     const string hit = "PlayerHit";
     const string death = "PlayerDeath";
+    const string deathDD = "PlayerDeathDD";
     const string revive = "PlayerRevive";
     const string counter = "PlayerCounter";
     const string heal = "PlayerHeal";
@@ -61,6 +62,33 @@ public class PlayerManager : MonoBehaviour
             spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, 1);
         if (status == 1 || status == 2) // continue moving after a parry
             player.canMove = true;
+
+        switch(lives){
+             case 4:
+                 if (CurrentHealth >= 100)
+            {
+                CurrentHealth = 100;
+            }
+                break;
+            case 3:
+                if (CurrentHealth >= 1)
+            {
+                CurrentHealth = 1;
+            }
+                break;
+            case 2:
+                if (CurrentHealth >= 1)
+            {
+                CurrentHealth = 1;
+            }
+                break;
+            case 1:
+                if (CurrentHealth >= 1)
+            {
+                CurrentHealth = 1;
+            }
+                break;
+        }
     }
 
     private void Awake()
@@ -169,25 +197,36 @@ public class PlayerManager : MonoBehaviour
         if (CurrentHealth <= 0)
         {
             lives--;
-            if (lives == 1)
+            if (lives == 3)
             {
                 dead = true;
                 //rb.simulated = false; character stays in air when he dies if these lines are active
                 player.enabled = false;
+                rb.velocity = new Vector2(0,0);                
                 player.ChangeAnimationState(death);
                 StartCoroutine(DeathDefiance());
-                CurrentHealth = (MaxHealth * 40) / 100;
+                CurrentHealth = (MaxHealth * 1) / 1;
 
+            }
+            if (lives == 2)
+            {
+                CurrentHealth = (MaxHealth * 1) / 100;
+              
+            }
+            if (lives == 1)
+            {
+                CurrentHealth = (MaxHealth * 1) / 100;
+               
             }
             if (lives == 0)
             {
                 dead = true;
-                player.ChangeAnimationState(death);
+                player.ChangeAnimationState(deathDD);
                 CurrentHealth = MaxHealth;
-                lives = 2;
+                lives = 4;
                 //rb.simulated = false; character stays in air when he dies if these lines are active
                 player.enabled = false;
-                StartCoroutine(RespawnPlayer());
+                //StartCoroutine(RespawnPlayer());
             }
         }
     }
