019c5502b55ab23e6384e7b3cb05c771cc9c3592
SAFAKAYA\safakaya <safaselahattin.kaya@ogr.iuc.edu.tr>
2022-05-26 13:18:00 +0300

    FakeWall prefab addded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -576,6 +576,8 @@ public class PlayerController : MonoBehaviour
                 enemy.GetComponent<Minion_wpoke>().TakeDamage(attackDamage);
             if(enemy.CompareTag("Legolas"))
                 enemy.GetComponent<Legolas>().TakeDamage(attackDamage);
+            if(enemy.CompareTag("FakeWall"))
+                enemy.GetComponent<FakeWallScript>().TakeDamage(attackDamage);
         }
         attackTime = 0f;
     }
